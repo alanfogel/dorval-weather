b692eb2654366d758b92b99641abecf1e63ef659
Alan Fogel <alanjfogel@gmail.com>
2025-07-22 14:03:05 -0600

    updated crontab script

--- add_cron.sh
+++ add_cron.sh
@@ -4,7 +4,7 @@ crontab -l 2>/dev/null > temp_cron || true
 
 cat <<EOF >> temp_cron
 # Take a picture every hour
-0 * * * * /usr/bin/python3 /home/madlab/dendro-pi-main/main/dendro_pictures.py
+*/15 * * * * /usr/bin/python3 /home/madlab/weatherstation/weather-station.py
 
 # Upload pictures to Dropbox once per day at 3am
 0 3 * * * bash /home/madlab/dendro-pi-main/upload-to-dropbox.sh
